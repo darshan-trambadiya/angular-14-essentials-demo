6e9278a91519c0938bcfdb2adbabb78146be9190
Darshan Trambadiya <trambadiyadarshan5@gmail.com>
2025-03-28 21:39:52 +0530

    Update project name & title

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/perx-angular-interview-exam'),
+      dir: require('path').join(__dirname, './coverage/angular-14-essentials-demo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
